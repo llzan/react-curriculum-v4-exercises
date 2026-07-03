9a9485f605732f5f1472e9f0a075d44c993d4de2
Lay Lay Zan <132317800+llzan@users.noreply.github.com>
2026-07-03 14:04:12 -0500

    lesson-01

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,25 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Lay Lay Zan';
+  const age = 27;
+
+  const hobbies = ['Hiking', 'Cooking', 'Reading', 'Traveling', 'Soccer'];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        I am {name}, and I am {age} years old. I am an asipiring UX/UI Designer
+        based in Saint Paul, Minnesota.
+      </p>
+      <h2>My Hobbies</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+// Screenshot of rendered page
